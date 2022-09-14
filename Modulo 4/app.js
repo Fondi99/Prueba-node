f031e5f876e801e4a19ee4a1f3f0449b60f16f11
Fondi99 <matiasfondini344@gmail.com>
2022-09-14 14:30:59 -0300

    Cambios al modulo 4

--- Modulo 4/app.js
+++ Modulo 4/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const app = express()
+const app = express();
+const path = require('path');
 
 //Abro servidor
 app.listen(3000,()=>
     console.log("Server running")
 );
-
+app.get('/index', (req, res) => {
+    let htmlPath = path.resolve(__dirname, './views/index.html')
+    res.sendFile(htmlPath)
+});
 //Pagina 404, envio pagina no encontrada
 app.get('/404', (req, res) => {
     res.send("Error página no encontrada")
